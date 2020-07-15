e3b5668564e45ea8ecdc4ad8ef5e35f81642b216
CHAITANYA REDDY <lvcr.dev@gmail.com>
2020-07-16 00:20:01 +0530

    fixed images bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./styles.css";
-import mylogo from "./mylogo.png";
+
 export default function App() {
   return (
     <div className="App">
       <h1> Hi, I'm Chaitanya </h1>
-      <img src={mylogo} alt="hellow" height={200} />
+      <img
+        src="https://i.ibb.co/QKFvbnX/mylogo.png"
+        alt="hellow"
+        height={200}
+      />
       <h2> Web , App Devloper</h2>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import Middle from "./middle";
 import Third from "./third";
-import pic1 from "./appdev.png";
-import pic2 from "./programming.png";
-import pic3 from "./Programming1.png";
+
 import Nav from "./navbar";
 import Fot from "./fotter";
 import Skill from "./skills";
@@ -25,7 +23,9 @@ ReactDOM.render(
       body={
         "I'm a App developer and i have experience in developing app for both IOS an Android using FLUTTER."
       }
-      pic={pic1}
+      pic={
+        "https://vrl-eu-cdn.wetransfer.net/ivise/eyJwaXBlbGluZSI6W1sic3JnYiIse31dLFsiYXV0b19vcmllbnQiLHt9XSxbImdlb20iLHsiZ2VvbWV0cnlfc3RyaW5nIjoiMTI4MHg4MDA_In1dLFsic2hhcnBlbiIseyJyYWRpdXMiOjAuNzUsInNpZ21hIjowLjV9XSxbImV4cGlyZV9hZnRlciIseyJzZWNvbmRzIjo2MDQ4MDB9XV0sInNyY191cmwiOiJzMzovL3dldHJhbnNmZXItZXUtcHJvZC1vdXRnb2luZy9kMTRhNjQ2OTc1NzQ2ZGE0YjNhYWQ1ZDk4YTM2NjczMjIwMjAwNzE1MTg0MDE2LzA2N2QzYWUzMWEzOTIyZThmNWZhMTg4MzA1MmRhYjk2ODE4YWY5NTAifQ/29094fe811078062207fa6e674ec103650d91fb30c71dc7860072b2c3d93b20d"
+      }
     />
     <Third
       colour={"#47CE65"}
@@ -33,7 +33,7 @@ ReactDOM.render(
       body={
         "I'm also a Web developer and have experience in HTMl CSS JavaScript and other frameworks like BootStrap and jQuery as well."
       }
-      pic={pic2}
+      pic={"https://i.ibb.co/Hr1qYvz/programming.jpg"}
     />
     <Third
       colour={"#F58330"}
@@ -41,7 +41,7 @@ ReactDOM.render(
       body={
         " I do a lot of competative codeing as well and I'm good at solving the questions with time limit ."
       }
-      pic={pic3}
+      pic={"https://i.ibb.co/XJGhNjp/Programming1.png"}
     />
     <Skill />
     <Fot />
